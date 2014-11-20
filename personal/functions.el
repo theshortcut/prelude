23a8a3574bc045a1431d9739484c4c62d52559f1
Clayton Ferris <cferris@gmail.com>
2014-11-20 16:45:18 -0700

    fix ensime hook and add a handful of functions to be used by evil-leader commands

---
+++ personal/functions.el
@@ -0,0 +1,121 @@
+;; found at http://emacswiki.org/emacs/KillingBuffers
+(defun kill-other-buffers ()
+  "Kill all other buffers."
+  (interactive)
+  (let (name (buffer-name))
+    (when (yes-or-no-p (format "Killing all buffers except \"%s\" ? " buffer-file-name))
+      (mapc 'kill-buffer (delq (current-buffer) (buffer-list)))
+      (message "Buffers deleted!"))))
+
+;; from https://gist.github.com/3402786
+(defun toggle-maximize-buffer ()
+  "Maximize buffer"
+  (interactive)
+  (if (= 1 (length (window-list)))
+      (jump-to-register '_)
+    (progn
+      (set-register '_ (list (current-window-configuration)))
+      (delete-other-windows))))
+
+(defun toggle-maximize-centered-buffer ()
+  "Maximize buffer and center it on the screen"
+  (interactive)
+  (if (= 1 (length (window-list)))
+      (progn  (bzg-big-fringe-mode 0)
+              (jump-to-register '_))
+    (progn
+      (set-register '_ (list (current-window-configuration)))
+      (delete-other-windows)
+      (bzg-big-fringe-mode 1))))
+
+;; from magnars
+(defun delete-current-buffer-file ()
+  "Removes file connected to current buffer and kills buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (ido-kill-buffer)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
+
+;; from magnars
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond ((get-buffer new-name)
+               (error "A buffer named '%s' already exists!" new-name))
+              (t
+               (rename-file filename new-name 1)
+               (rename-buffer new-name)
+               (set-visited-file-name new-name)
+               (set-buffer-modified-p nil)
+               (message "File '%s' successfully renamed to '%s'" name (file-name-nondirectory new-name))))))))
+
+;; from https://github.com/gempesaw/dotemacs/blob/emacs/dg-defun.el
+(defun kill-matching-buffers-rudely (regexp &optional internal-too)
+  "Kill buffers whose name matches the specified REGEXP. This
+function, unlike the built-in `kill-matching-buffers` does so
+WITHOUT ASKING. The optional second argument indicates whether to
+kill internal buffers too."
+  (interactive "sKill buffers matching this regular expression: \nP")
+  (dolist (buffer (buffer-list))
+    (let ((name (buffer-name buffer)))
+      (when (and name (not (string-equal name ""))
+                 (or internal-too (/= (aref name 0) ?\s))
+                 (string-match regexp name))
+        (kill-buffer buffer)))))
+
+;; from http://dfan.org/blog/2009/02/19/emacs-dedicated-windows/
+(defun toggle-current-window-dedication ()
+  "Toggle dedication state of a window."
+  (interactive)
+  (let* ((window    (selected-window))
+         (dedicated (window-dedicated-p window)))
+    (set-window-dedicated-p window (not dedicated))
+    (message "Window %sdedicated to %s"
+             (if dedicated "no longer " "")
+             (buffer-name))))
+
+;; from magnars modified by ffevotte for dedicated windows support
+(defun rotate-windows (count)
+ "Rotate your windows.
+Dedicated windows are left untouched. Giving a negative prefix
+argument takes the kindows rotate backwards."
+ (interactive "p")
+ (let* ((non-dedicated-windows (remove-if 'window-dedicated-p (window-list)))
+        (num-windows (length non-dedicated-windows))
+        (i 0)
+        (step (+ num-windows count)))
+   (cond ((not (> num-windows 1))
+          (message "You can't rotate a single window!"))
+         (t
+          (dotimes (counter (- num-windows 1))
+            (let* ((next-i (% (+ step i) num-windows))
+
+                   (w1 (elt non-dedicated-windows i))
+                   (w2 (elt non-dedicated-windows next-i))
+
+                   (b1 (window-buffer w1))
+                   (b2 (window-buffer w2))
+
+                   (s1 (window-start w1))
+                   (s2 (window-start w2)))
+              (set-window-buffer w1 b2)
+              (set-window-buffer w2 b1)
+              (set-window-start w1 s2)
+              (set-window-start w2 s1)
+              (setq i next-i)))))))
+
+(defun rotate-windows-backward (count)
+ "Rotate your windows backward."
+  (interactive "p")
+  (rotate-windows (* -1 count)))
--- personal/scala.el
+++ personal/scala.el
@@ -1,2 +1,4 @@
 (require 'packages)
+(require 'prelude-programming)
 (remove-hook 'scala-mode 'flycheck-mode)
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
